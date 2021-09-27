fdbbbcd41152e9fe37ac04df2972e3e5b5dc2854
jaredmauck <jaredmauck@gmail.com>
2021-09-26 18:00:11 -0700

    adding some extra interesting 'for loops'

--- for_loop.py
+++ for_loop.py
@@ -1,11 +1,15 @@
-string_1 = "supercalifragilisticexpialidocious"
+string_1 = "supercalifragilisticexpialidocious" #will print letter by letter in for loop
 
-list_linux = ["ubuntu", "redhat", "centos", "raspi"]
+list_linux = ["ubuntu", "redhat", "centos", "raspi"] #will print each list item in for loop
+
+text_file = 'apples\npears\nbananas\nstrawberries'.splitlines() #will print line by line in for loop
 
 #"character" can be anything
 for character in string_1:
     print(character)
-#"xyz" same as "character" from above
-for xyz in list_linux:
-    print(xyz)
+
+for item in list_linux:
+    print(item)
     
+for lines in text_file:
+    print(lines)
