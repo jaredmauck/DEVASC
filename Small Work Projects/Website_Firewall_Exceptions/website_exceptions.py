9cdb2d01b1b4be6b5aeac6dd9f49250c831cfb89
Jared Mauck <45055567+jaredmauck@users.noreply.github.com>
2021-09-27 11:11:06 -0700

    using a function for the initial read

--- Small Work Projects/Website_Firewall_Exceptions/website_exceptions.py
+++ Small Work Projects/Website_Firewall_Exceptions/website_exceptions.py
@@ -1,8 +1,9 @@
-with open('websites.txt', 'r') as f:
-    sites = f.read().splitlines()
-
+def fetch_line_items():
+    with open('websites.txt', 'r') as sites:
+        web_sites = sites.read().splitlines()
+        return web_sites
 nl = '\n'
-
-for site in sites:
-    with open('websites.txt', 'a') as f:
-        f.write(f'{nl}{site}/*{nl}*.{site}{nl}*.{site}/*')
+line_items = fetch_line_items()
+for items in line_items:
+    with open('input.txt', 'a') as f:
+        f.write(f'{nl}{items}/*{nl}*.{items}{nl}*.{items}/*')
