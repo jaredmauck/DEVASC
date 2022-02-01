c06857abc9e2279bd83c4a0a7f10a26ebabfac49
jaredmauck <jaredmauck@gmail.com>
2022-02-01 12:38:05 -0800

    updates

--- nick-russo/decode-webpage/main.py
+++ nick-russo/decode-webpage/main.py
@@ -1,17 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+# from bs4 import SoupStrainer
+import re
 
+# url = "https://www.recorderonline.com/"
 # url = "https://nytimes.com/"
-url = "https://artofnetworkengineering.com/category/podcast/"
+# url = "https://artofnetworkengineering.com/category/podcast/"
+url = "http://njrusmc.net/"
 
 payload={}
 headers = {}
 
 response = requests.request("GET", url, headers=headers, data=payload)
-
-
+# only_a_tags = SoupStrainer("a")
+regex_class = re.compile(r'.*')
+regex = re.compile(r'^h[1-9]')
 soup = BeautifulSoup(response.text, "html.parser")
-header = soup("h2", {"class": "entry-title"})
+heading = soup(regex, {"class": regex_class})
+if heading == []:
+    heading = soup(regex)
 
-for h in header:
-    print(h.a.string)
+for h in heading:
+    try:
+        if h.a is None and h is None:
+            pass
+        elif h.a is not None:
+            print(h.a.string)
+        elif h is not None:
+            print(h.string)
+        else:
+            print(h)
+    except:
+        pass
